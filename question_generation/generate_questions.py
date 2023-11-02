3232881306886e30b614d3da806c99bffb83f7f1
netien <niels.etien@insa-rouen.fr>
2023-11-02 11:42:05 +0900

    cleaning trailing whitespaces and vocabulary

--- question_generation/generate_questions.py
+++ question_generation/generate_questions.py
@@ -3,16 +3,15 @@ import sys
 import random
 import argparse
 
-
 BASE_QUESTION_TEMPLATE = """I have a secret message.
 I used the following encryption methods:
 {encryption_methods}
-And finally got this encrypred result:
+And finally got this encrypted result:
 {encrypted_msg}
-Can you help me decypher the original message?
+Can you help me decipher the original message?
 """
 
-BASE_ANSWER_TEMPLATE = """Let's decrypt it step by step:
+BASE_ANSWER_TEMPLATE = """Let's decipher it step by step:
 {step_by_step_decryption}
 So the original message was:
 {original_message}
@@ -32,65 +31,65 @@ def random_gibberish_message():
     for _ in range(num_chars):
         msg += chr(random.randint(ord("a"), ord("z")))
     return msg
-    
+
 
 class EncryptionMethod:
-    
-    def __init__(self, description, func, inv_func, charecterwise=False, assertions_to_make=5):
+
+    def __init__(self, description, func, inv_func, characterwise=False, assertions_to_make=5):
         self.description = description
         self.func = func
         self.inv_func = inv_func
-        self.charecterwise = charecterwise 
+        self.characterwise = characterwise
         for _ in range(assertions_to_make):
             test_msg = random_gibberish_message()
-            if self.decrypt_msg(self.encrypet_msg(test_msg)) != test_msg:
+            if self.decrypt_msg(self.encrypt_msg(test_msg)) != test_msg:
                 print(self)
-                print(f"Failed sanety check for {test_msg=}, {self.encrypet_msg(test_msg)=}, {self.decrypt_msg(self.encrypet_msg(test_msg))=}.")
-    
-    def encrypet_msg(self, msg):
-        if self.charecterwise:
+                print(f"Failed sanity check for {test_msg=}, {self.encrypt_msg(test_msg)=}, {self.decrypt_msg(self.encrypt_msg(test_msg))=}.")
+
+    def encrypt_msg(self, msg):
+        if self.characterwise:
             encrypted = ""
             for c in msg:
                 encrypted += self.func(c)
         else:
             encrypted = self.func(msg)
         return encrypted
-    
+
     def decrypt_msg(self, msg):
-        if self.charecterwise:
+        if self.characterwise:
             decrypted = ""
             for c in msg:
                 decrypted += self.inv_func(c)
         else:
             decrypted = self.inv_func(msg)
         return decrypted
-    
+
     def __str__(self):
-        return f"{self.description} For example abcioeuzxy becomes {self.encrypet_msg('abcioeuzxy')}"
+        return f"{self.description} For example abcioeuzxy becomes {self.encrypt_msg('abcioeuzxy')}"
 
 ENCRYPTION_METHODS_GROUPS = [
     [
         EncryptionMethod(
-            f"Caesar cipher or shift cipher with a distance of {i}, shifting every letter to the one {i} after her cyclically.",
+            f"Caesar cipher or shift cipher with a distance of {i}, shifting every letter to the one {i} after it cyclically.",
             lambda char: chr(ord("a") + ((ord(char) - ord("a") + i ) % 26 )),
             lambda char: chr(ord("a") + ((ord(char) - ord("a") - i ) % 26 )),
-            charecterwise=True
+            characterwise=True
         )
         for i in range(-12, 14)
     ],
     [
         EncryptionMethod(
-            "Swapping adjaset charecters, First one swaps with second one, third with forth and so on. ",
+            "Swapping adjacent characters, first one swaps with second one, third with forth and so on.",
             lambda string: "".join(string[i + (-1) ** i] for i in range(2*int(len(string)/2))) + (string[-1] if len(string) % 2 else ""),
             lambda string: "".join(string[i + (-1) ** i] for i in range(2*int(len(string)/2))) + (string[-1] if len(string) % 2 else "")
         ),
         EncryptionMethod(
-            "Inverse order, reading the message from end to start. ",
+            "Inverse order, reading the message from end to start.",
             lambda string: string[::-1],
             lambda string: string[::-1]
         ),
         EncryptionMethod(
-            "Swapping first half of the message with the second half. ",
+            "Swapping first half of the message with the second half.",
             lambda string: string[int(len(string) / 2):] + string[:int(len(string) / 2)],
             lambda string: string[int(len(string) / 2):] + string[:int(len(string) / 2)]
         )
@@ -108,15 +107,15 @@ ENCRYPTION_METHODS_GROUPS = [
 
 def generate_question(args):
     amount_of_cyphers = args.cyphers_per_question + random.randint(0,  args.cyphers_ammount_variation)
-    cyphers = [random.choice(random.choice(ENCRYPTION_METHODS_GROUPS)) 
+    cyphers = [random.choice(random.choice(ENCRYPTION_METHODS_GROUPS))
                for _ in range(amount_of_cyphers)]
     if args.message_mode == "gibberish":
         msg = random_gibberish_message()
-    
+
     encrypted_stages = [msg]
     for cypher in cyphers:
-        encrypted_stages.append(cypher.encrypet_msg(encrypted_stages[-1]))
-    
+        encrypted_stages.append(cypher.encrypt_msg(encrypted_stages[-1]))
+
     question = BASE_QUESTION_TEMPLATE.format(
         encryption_methods="\n".join([f"{i+1}. {cypher}" for i, cypher in enumerate(cyphers)]),
         encrypted_msg=encrypted_stages[-1]
@@ -130,7 +129,7 @@ def generate_question(args):
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='Children cypher decryption Dataset Question generator')
+    parser = argparse.ArgumentParser(prog='Children cipher decipherment Dataset Question generator')
     parser.add_argument('--cyphers_per_question', type=int, default=2,
                         help='Base amount of cyphers per question')
     parser.add_argument('--cyphers_ammount_variation', type=int, default=0,
